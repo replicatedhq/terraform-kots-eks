9952e79d4832e7a0902cade5769a731580ab88e8
natty-li <natasha.li@fishtownanalytics.com>
2021-06-14 16:19:52 -0600

    reorganizing and renaming

--- helm.tf
+++ helm.tf
@@ -6,6 +6,18 @@ provider "helm" {
   }
 }
 
+resource "helm_release" "cluster_rbac" {
+  name  = "cluster-rbac"
+  chart = "${path.module}/local_charts/cluster-rbac"
+
+  namespace = var.existing_namespace ? var.custom_namespace : kubernetes_namespace.dbt_cloud.0.metadata.0.name
+
+  set {
+    name  = "namespace"
+    value = var.existing_namespace ? var.custom_namespace : kubernetes_namespace.dbt_cloud.0.metadata.0.name
+  }
+}
+
 resource "helm_release" "kube_cleanup_operator" {
   count = var.enable_kube_cleanup_operator ? 1 : 0
 
@@ -165,16 +177,4 @@ resource "helm_release" "datadog" {
     value = "256Mi"
   }
 
-}
-
-resource "helm_release" "cluster_rbac" {
-  name  = "cluster-rbac"
-  chart = "${path.module}/local_charts/cluster-rbac"
-
-  namespace = var.existing_namespace ? var.custom_namespace : kubernetes_namespace.dbt_cloud.0.metadata.0.name
-
-  set {
-    name  = "rbacNamespace"
-    value = var.existing_namespace ? var.custom_namespace : kubernetes_namespace.dbt_cloud.0.metadata.0.name
-  }
 }

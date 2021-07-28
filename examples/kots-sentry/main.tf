3e808645dfb297b0e30565f3ff96b3dc20819523
dexhorthy <dexter@replicated.com>
2021-07-28 10:59:39 -0500

    Some random cleanup

--- examples/kots-sentry/main.tf
+++ examples/kots-sentry/main.tf
@@ -3,7 +3,6 @@ provider "aws" {
   region  = var.region
 }
 
-
 locals {
   full_name = "kots-sentry-${var.namespace}-${var.environment}"
 }
@@ -12,12 +11,13 @@ module "terraform-kots-eks" {
 
   source = "../../terraform-kots-eks"
 
-  region            = "us-east-1"
+  region            = var.region
   vpc_id            = module.vpc.vpc_id
   cidr_block        = var.cidr_block
   enable_bastion    = false
   private_subnets   = module.vpc.private_subnets
   public_subnets    = module.vpc.public_subnets
+  creation_role_arn = ""
 
   app_slug    = "kots-sentry"
   namespace   = var.namespace
@@ -26,9 +26,32 @@ module "terraform-kots-eks" {
   k8s_node_count = 2
   k8s_node_size  = "t3.xlarge"
 
-  custom_namespace       = var.k8s_namespace
-  existing_namespace     = !var.create_k8s_namespace
-  license_file_path      = var.license_file_path
-  admin_console_password = var.admin_console_password
+  custom_namespace          = var.k8s_namespace
+  existing_namespace        = !var.create_k8s_namespace
+  license_file_path         = var.license_file_path
+  admin_console_password    = var.admin_console_password
+  admin_console_config_yaml = <<EOT
+apiVersion: kots.io/v1beta1
+kind: ConfigValues
+metadata:
+  name: kots-sentry
+spec:
+  values:
+    admin_username:
+      value: ${var.sentry_admin_username}
+    admin_password:
+      value: ${var.sentry_admin_password}
+status: {}
+EOT
 
+}
+
+output "next_steps" {
+  value = <<EOT
+Run
+
+    kubectl kots admin-console --namespace ${var.k8s_namespace}"
+
+To access the application
+EOT
 }
